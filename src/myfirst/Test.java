99cba852ab7cc128af62397f50c7c88b058d0def
Nykolai Kuteinykov <nykolai.kuteinykov@i.ua>
2018-03-12 21:51:39 +0200

    2 step

--- src/myfirst/Test.java
+++ src/myfirst/Test.java
@@ -4,5 +4,6 @@ package myfirst;
 public class Test {
     public static void main(String[] args ){
         System.out.println("Hello World !!!");
+        System.out.println("2 step !!!");
     }
 }

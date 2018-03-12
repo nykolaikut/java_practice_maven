385e89b292f5992f98f25c5d38aa6ad59dafaac8
Nykolai Kuteinykov <nykolai.kuteinykov@i.ua>
2018-03-12 22:36:26 +0200

    3 commit

--- src/myfirst/Test.java
+++ src/myfirst/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args ){
         System.out.println("Hello World !!!");
         System.out.println("2 step !!!");
+        System.out.println("3 step !!!");
     }
 }

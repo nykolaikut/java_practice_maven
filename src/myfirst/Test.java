4f6c0fdbbe70212023043ca083ac4027ed67b9af
nykolaikut <nykolai.kuteinykov@i.ua>
2018-03-13 16:21:23 +0200

    Update Test.java

--- src/myfirst/Test.java
+++ src/myfirst/Test.java
@@ -6,7 +6,7 @@ public class Test {
         System.out.println("2 step !!!");
         System.out.println("3 step !!!");
         System.out.println("4 step !!!");
-        System.out.println("7990 555 666 77 step !!!");
+        System.out.println("7990 555 666 77 88 step !!!");
 
     }
 }

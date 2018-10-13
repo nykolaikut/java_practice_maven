cffc26a3e3dc4dfe3b21115f7a557f24781599c2
Nykolai Kuteinykov <nykolai.kuteinykov@i.ua>
2018-10-13 07:33:42 +0300

    with Checkstyle

--- src/main/java/ua/education/Main.java
+++ src/main/java/ua/education/Main.java
@@ -1,8 +1,12 @@
 package ua.education;
 
+/**
+ * Hello world!
+ */
 public class Main {
+    /*
+     */
     public static void main(String[] args) {
         System.out.println("My first maven-project");
-
     }
 }

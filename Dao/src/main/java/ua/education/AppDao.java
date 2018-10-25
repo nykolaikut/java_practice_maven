a7744ec3fec847ec3fc043b035b6426852b9532c
Nykolai Kuteinykov <nykolai.kuteinykov@i.ua>
2018-10-26 00:37:22 +0300

    add AppCore

---
+++ Dao/src/main/java/ua/education/AppDao.java
@@ -0,0 +1,10 @@
+package ua.education;
+
+/**
+ * Dao module.
+ */
+public class AppDao {
+    public static void main(String[] args) {
+        System.out.println("Dao module");
+    }
+}
--- src/main/java/ua/education/Main.java
+++ src/main/java/ua/education/Main.java
@@ -4,8 +4,9 @@ package ua.education;
  * Hello world!
  */
 public class Main {
-    /*
-     */
+    /**
+    *Comment.
+    */
     public static void main(String[] args) {
         System.out.println("My first maven-project");
     }
